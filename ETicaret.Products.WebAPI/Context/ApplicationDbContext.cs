c52313e15fee77433f4dc0c148428d550cfebce0
BeyzaÖzeren <Beyzazeren@DESKTOP-S0T0HBD>
2026-01-18 01:10:00 +0300

    Proje dosyası ekle.

---
+++ ETicaret.Products.WebAPI/Context/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ETicaret.Products.WebAPI.Context
+{
+    public sealed class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
---
+++ ETicaret.Products.WebAPI/Program.cs
@@ -0,0 +1,14 @@
+using ETicaret.Products.WebAPI.Context;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer"));
+});
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
