3db512f00d8ba410c7da25002c6772bc68a4b7a6
edabeyza <edabeyzaozeren@gmail.com>
2026-01-18 03:59:36 +0300

    products done

---
+++ ETicaret.ShoppingCarts.WebAPI/Context/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ETicaret.ShoppingCarts.WebAPI.Context
+{
+    public sealed class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
